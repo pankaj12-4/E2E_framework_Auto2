2466480450fce21ded3b69f1b6bccc9465656131
singh <singhpankajp301@gmail.com>
2025-12-07 14:57:44 +0530

    updated

--- Aristocrat/conftest.py
+++ Aristocrat/conftest.py
@@ -3,8 +3,6 @@ import tempfile
 import pytest
 import yaml
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
 
 @pytest.fixture(scope="session")
 def config():
@@ -18,30 +16,3 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.quit()
-
-# @pytest.fixture
-# def driver():
-#     chrome_options = Options()
-#
-#     # 1) Completely new temporary profile (no saved passwords)
-#     temp_profile_dir = tempfile.mkdtemp()
-#     chrome_options.add_argument(f"--user-data-dir={temp_profile_dir}")
-#
-#     # 2) Disable password manager + its bubbles
-#     prefs = {
-#         "credentials_enable_service": False,
-#         "profile.password_manager_enabled": False,
-#     }
-#     chrome_options.add_experimental_option("prefs", prefs)
-#
-#     # 3) Extra flags to avoid password / leak popups
-#     chrome_options.add_argument("--disable-save-password-bubble")
-#     chrome_options.add_argument(
-#         "--disable-features=PasswordManagerOnboarding,PasswordLeakDetection"
-#     )
-#
-#     driver = webdriver.Chrome(options=chrome_options)
-#     driver.maximize_window()
-#     yield driver
-#     driver.quit()
-
